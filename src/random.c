bca6d8da69c3f60be992a6cc41ee88029ce8d855
mati <mati@mati-VPCEG3WFX.(none)>
2014-05-17 11:47:47 -0300

    Update random library

--- src/random.c
+++ src/random.c
@@ -12,17 +12,17 @@ randomize(void)
 double
 randnormalize (void)
 {
-        return rand()/(double)RAND_MAX; /*Devuelve un numero entre 0 y 1(si quisiera sin incluir sumarle 1 a RAND)*/
+        return rand()/((double)RAND_MAX+1); /*Devuelve un numero entre 0 y 1(sin incluir)*/
 }
 
 int
 randint (int izq, int der)
 {
-        return (int)(randnormalize()*(der-izq)+izq); /*Devuelve un numero entero entre izq y der*/
+        return (int)(randnormalize()*(der-izq+1)+izq); /*Devuelve un numero entero entre izq y der*/
 }
 
 double
-randreal(double izq, double der)                        /*Devuelve un real entre izq y der*/
+randreal(double izq, double der)                        /*Devuelve un real entre izq y der(sin incluir)*/
 {
         return (randnormalize()*(der-izq)+izq);
 }
